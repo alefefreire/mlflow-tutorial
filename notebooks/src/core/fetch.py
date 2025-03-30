367889f1e77856f6cb5d6e8fc8e2cb96df4eb33d
Alefe Almeida <alefeofdealmeida@gmail.com>
2025-03-30 20:47:39 -0300

    improves docstring

--- notebooks/src/core/fetch.py
+++ notebooks/src/core/fetch.py
@@ -11,16 +11,10 @@ class Fetch(ABC):
     @abstractmethod
     def fetch(self) -> Dataset:
         """
-        Fetch the data based on
-        Parameters
-        ----------
-        *args : tuple
-            Positional arguments.
-        **kwargs : dict
-            Keyword arguments.
+        Base method to fetch the data.
         Returns
         -------
-        Any
-            The fetched data.
+        Dataset
+            The fetched dataset.
         """
         pass
