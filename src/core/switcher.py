724dcd2b7fc557ca63744cda9cbe7808040cbfcb
Alefe Almeida <alefeofdealmeida@gmail.com>
2025-05-10 19:57:47 -0300

    improving documentation

--- src/core/switcher.py
+++ src/core/switcher.py
@@ -8,11 +8,36 @@ class ModelSwitcher(BaseEstimator):
         self, estimator: BaseEstimator = DummyClassifier(strategy="most_frequent")
     ):
         """
-        A Custom BaseEstimator that can switch between sklearn-models.
+        A custom BaseEstimator that allows switching between different scikit-learn models.
+
+        This class is particularly useful when used in a scikit-learn Pipeline, as it enables
+        dynamic model selection during hyperparameter tuning with tools like GridSearchCV or
+        RandomizedSearchCV. By wrapping the estimator, it provides a unified interface for
+        fitting, predicting, and scoring, while allowing the flexibility to switch models
+        without modifying the pipeline structure.
+
         Parameters
         ----------
-        estimator : BaseEstimator
-            The model to be used.
+        estimator : BaseEstimator, optional
+            The scikit-learn model to be used. Default is DummyClassifier with the "most_frequent" strategy.
+
+        Methods
+        -------
+        fit(X, y=None, **kwargs)
+            Fits the selected model to the provided data.
+        predict(X, y=None)
+            Makes predictions using the selected model.
+        predict_proba(X)
+            Predicts class probabilities using the selected model (if supported).
+        score(X, y)
+            Returns the score of the selected model on the provided test data and labels.
+
+        Notes
+        -----
+        - This class is designed to work seamlessly with scikit-learn Pipelines, enabling
+          model selection and hyperparameter tuning in a structured and efficient manner.
+        - When used in a Pipeline, the `estimator` parameter can be set as part of the
+          parameter grid for GridSearchCV or RandomizedSearchCV, allowing automated model selection.
         """
 
         self.estimator = estimator
